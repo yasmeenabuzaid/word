3978395f6ee66bc544464915e863e3272248ed60
yasmeenabuzaid <yasmeenabuzaid552@gmail.com>
2024-10-20 13:52:47 +0300

    project

---
+++ wp-content/plugins/cartflows/admin-core/assets/build/editor-app.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7c1bc3304525896448af');
---
+++ wp-content/plugins/cartflows/admin-core/assets/build/settings-app.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'df0e50b4f4f6e2b8481a');
